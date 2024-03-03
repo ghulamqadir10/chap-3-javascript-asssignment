69014dc3195945a193a0fd85839a5260b7836b78
Ghulam qadir <154546196+ghulamqadir10@users.noreply.github.com>
2024-03-03 17:24:38 +0500

    Add files via upload

---
+++ chap3.js
@@ -0,0 +1,17 @@
+// assignment 03
+let my_age = 20;
+// alert(" i am " + my_age + years old");
+alert("i am 20 years old");
+
+let birth_year;
+birth_year=2003;
+alert("my birth year is " + birth_year)
+document.write("<h2 class='chap3'>My birth year is 2003");
+document.write("<h2 class='chap3'>Data type of my  ddeclared variable is number" + "<br>"  + "<br>" +"<br>");
+let visitor_info;
+visitor_name="john Doe";
+product_title= "T-shirt(s)";
+Quantity= 5
+console.log(visitor_info)
+
+document.write("<h2 class='chap3'>john Doe ordered 5 T-shirt(s) on XYZ Clothing store" + "<br>" + "<br>" + "<br>" + "<br>")
